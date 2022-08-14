566da83f268c2d811b5d45574a59493263bb158f
SoHenriquez <estebansotohenriquez@gmail.com>
2022-08-14 16:33:03 -0400

    Se comenta código de inicio y se crea apartado para el nav

--- inicio/inicio.php
+++ inicio/inicio.php
@@ -1,43 +1,28 @@
 <?php require_once('../partes/head.php') ?>
 <?php require_once('../controlador/read.php') ?>
-
-    <nav class="navbar navbar-expand-sm navbar-light bg-dark position-relative">
-        <div class="container-fluid ">
-            <div><img src="../img/logoACv2.svg"  alt="" srcset=""></div>
-            <a class="navbar-brand text-light position-absolute" href="#"></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID"
-                aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse " id="navbarID">
-                <div class="navbar-nav">
-                    <a class="nav-link active text-light" aria-current="page" href="http://localhost/phpmyadmin/index.php?route=/sql&server=1&db=lista&table=lista&pos=0">BaseDatos</a>
-                    <a class="nav-link active text-light" href="">Categoría</a>
-                    <a class="nav-link active text-light" href="">Lista</a>
-                    <a class="nav-link active text-light" href="">Clothes</a>
-                </div>
-            </div>
-            <!-- Reloj Digital -->
-            <div class="d-grid bg-dark border rounded-2" style="color:#0d8ab8; place-items: center; font-size: 45px; font-family: 'Bungee Shade', cursive;cursor:default;" id="clock"></div>
-        </div>
-        
-    </nav>
+<!-- Inicio NAVBAR -->
+<?php include '../partes/nav.php'; ?>
+<!-- Fin NAVBAR -->
     <div class="container-fluid bg-gradient-light rounded-3 ">
 
         <div class="row bg-light ">
             
                 
-            
+            <!-- COL.1 -->
             <div class="col-2 d-flex align-items-md-start justify-content-center"><img src="../img/logoAC.svg" width="225px" height="200" alt="" srcset=""></div>
+            <!-- INICIO LISTADO COL.2-->
             <div class="col-7 bg-dark border rounded-2 mt-1" style="box-shadow:  25px 0 50px #888888,-25px 0 50px #888888;">
+                <!-- Titulo -->
                 <div class="text-center mt-1 ml-4"><h1 class="rounded" style="color:red; place-items: center; font-size: 45px; font-family: 'Bungee Shade', cursive; box-shadow: 0px 10px 45px -7px rgba(250,250,250,0.47);" >Lista de Tareas</h1></div>
                 <br>
+                <!-- Botones agregar y UBB -->
                 <div class="row">
                     <div class="col-4"></div>
                     <div class="col-2 me-3"><button class="btn btn-secondary w-100" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-plus"></i> Agregar</button></div>
                     <div class="col-2"><button class="btn btn-secondary w-100"><i class="fa-solid fa-plus"></i> Tarea UBB</button></div>
                     
                 </div>
+                <!-- INCICIO TABLA LISTADO -->
                 <table id="orden" class="table table-borderless">
                     <thead class="bg-danger">
                         <tr >
@@ -83,19 +68,22 @@
                         <div id="divModal"> </div>
                     </tbody>
                 </table>
+                <!-- FIN TABLA LISTADO -->
                 <br>
             </div>
+            <!-- FIN LISTADO -->
+
+            <!-- INICIO CALENDARIO COL.3-->
             <div class="col-3 px-5">
                 <!-- Calendario -->
                 <div class="" id='calendar'></div>
             </div>
+            <!-- FIN COL.3 CALENDARIO -->
         </div>
         
     </div>
     
-    
-    
-    
+<!-- INCLUDES -->
 <?php include '../partes/modal_modificar.php'; ?>
 <?php include '../partes/modal.php'; ?>
 <?php include '../partes/script.php'; ?>
---
+++ partes/nav.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-sm navbar-light bg-dark position-relative">
+        <div class="container-fluid ">
+            <div><img src="../img/logoACv2.svg"  alt="" srcset=""></div>
+            <a class="navbar-brand text-light position-absolute" href="#"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID"
+                aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse " id="navbarID">
+                <div class="navbar-nav">
+                    <a class="nav-link active text-light" aria-current="page" href="http://localhost/phpmyadmin/index.php?route=/sql&server=1&db=lista&table=lista&pos=0">BaseDatos</a>
+                    <a class="nav-link active text-light" href="">Categoría</a>
+                    <a class="nav-link active text-light" href="">Lista</a>
+                    <a class="nav-link active text-light" href="">Clothes</a>
+                    <a class="nav-link active text-light" href="">Hábitos Atómicos</a>
+                </div>
+            </div>
+            <!-- Reloj Digital -->
+            <div class="d-grid bg-dark border rounded-2" style="color:#0d8ab8; place-items: center; font-size: 45px; font-family: 'Bungee Shade', cursive;cursor:default;" id="clock"></div>
+        </div>
+        
+    </nav>
